3d98b456bbfdcb79994321caa29004cf9e2aef86
MeltingSlime <113958970+MeltingSlime@users.noreply.github.com>
2023-02-13 22:44:44 -0800

    tdxt change middle 1

--- Assets/Editor/TriggerObjectLock.cs
+++ Assets/Editor/TriggerObjectLock.cs
@@ -2,8 +2,8 @@ using UnityEditor;
 
 static class TriggerObjectLock
 {
-
-    [MenuItem("Tools/Toggle Inspector Lock %l")] // Ctrl + L
+    //To create a hotkey, use the following special characters: % (ctrl on Windows and Linux, cmd on macOS), ^ (ctrl on Windows, Linux, and macOS), # (shift), & (alt). If no special modifier key combinations are required, the key can be given after an underscore. For example, to create a menu with the hotkey Shift+Alt+G, use "MyMenu/Do Something #&g". To create a menu with hotkey G and no key modifiers pressed, use "MyMenu/Do Something _g".
+    [MenuItem("Tools/Toggle Inspector Lock _1")] // #5 is numberpad 5, ctrl + l is %l, # is shift
     static void ToggleInspectorLock()
     {
         ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
