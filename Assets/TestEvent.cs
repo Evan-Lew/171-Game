dbf2b842a2986df8c33ae69cc225a160aeb83957
MeltingSlime <113958970+MeltingSlime@users.noreply.github.com>
2023-02-21 10:19:49 -0800

    1

--- Assets/TestEvent.cs
+++ Assets/TestEvent.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class TestEvent : MonoBehaviour
 {
-
     private void Awake()
     {
         Debug.Log("你妈死了");
+        
     }
     // Start is called before the first frame update
     void Start()
