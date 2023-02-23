fbbddd41f4325adfa43df2037e8d265e7c70da27
MeltingSlime <113958970+MeltingSlime@users.noreply.github.com>
2023-02-23 12:57:32 -0800

    1

--- Assets/Scripts/Camp/DeckEditSystem.cs
+++ Assets/Scripts/Camp/DeckEditSystem.cs
@@ -13,15 +13,16 @@ public class DeckEditSystem : MonoBehaviour
     [SerializeField] Transform Pos_DisplayCardCandidates, Pos_CandidatesMin, Pos_CandidatesMax, Pos_CardsDisplay;
     [SerializeField] Card CardPrefab;
     [SerializeField] List<Card_Basedata> CardCandidatesList = new();
-    List<Card> Cards_FromCandidates = new();
-    List<Vector3> CardCandidatesPositionList = new();
+    List<Card> Cards_ForPick = new();
+    [SerializeField] int numberCandidate = 3;
+    List<Vector3> CardForPickPositionList = new();
     [SerializeField] float candidateDistance;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpawnCandidates();
+            //SpawnCandidates();
         }
     }
 
@@ -30,18 +31,28 @@ public class DeckEditSystem : MonoBehaviour
     public void ResetCandidate()
     {
         CardCandidatesList.Clear();
-        CardCandidatesPositionList.Clear();
-        Cards_FromCandidates.Clear();
+        CardForPickPositionList.Clear();
+        Cards_ForPick.Clear();
+    }
+
+    public void DestroyCurrentCardsForPick()
+    {
+        foreach(Card card in Cards_ForPick)
+        {
+            Destroy(card.gameObject);
+        }
+        Cards_ForPick.Clear();
     }
 
     //this function  will be called when ever you have a group of card_database wants to generate
-    public void SpawnCandidates()
+    public void SpawnCardsForPick()
     {
         Debug.Log("Hello");
-        Cards_FromCandidates.Clear();
-        for (int i = 0; i < CardCandidatesList.Count; i++)
+        DestroyCurrentCardsForPick();
+        
+        for (int i = 0; i < numberCandidate; i++)
         {
-            InstantiateCandidateCard(CardPrefab, CardCandidatesList[i], Pos_DisplayCardCandidates);
+            InstantiateCandidateCard(CardPrefab, CardCandidatesList[Random.Range(0, CardCandidatesList.Count)], Pos_DisplayCardCandidates);
         }
         SetCandidateCardPos();
     }
@@ -49,27 +60,27 @@ public class DeckEditSystem : MonoBehaviour
     void SetCandidateCardPos()
     {
         Vector3 distanceBetween;
-        CardCandidatesPositionList.Clear();
-        if (CardCandidatesList.Count - 1 == 0)
+        CardForPickPositionList.Clear();
+        if (Cards_ForPick.Count - 1 == 0)
         {
             distanceBetween = (Pos_CandidatesMax.position - Pos_CandidatesMin.position) / 2;
-            for (int i = 0; i < CardCandidatesList.Count; i++)
+            for (int i = 0; i < Cards_ForPick.Count; i++)
             {
-                Cards_FromCandidates[i].CandidatePosition = i;
-                Cards_FromCandidates[i].cardState = Card.state.DeckCandidate;
-                CardCandidatesPositionList.Add(Pos_CandidatesMin.position + (distanceBetween));
-                Cards_FromCandidates[i].MoveToPoint(CardCandidatesPositionList[i], Cards_FromCandidates[i].transform.rotation);
+                Cards_ForPick[i].CandidatePosition = i;
+                Cards_ForPick[i].cardState = Card.state.DeckCandidate;
+                CardForPickPositionList.Add(Pos_CandidatesMin.position + (distanceBetween));
+                Cards_ForPick[i].MoveToPoint(CardForPickPositionList[i], Cards_ForPick[i].transform.rotation);
             }
         }
         else
         {
             distanceBetween = (Pos_CandidatesMax.position - Pos_CandidatesMin.position) / (CardCandidatesList.Count - 1);
-            for (int i = 0; i < CardCandidatesList.Count; i++)
+            for (int i = 0; i < Cards_ForPick.Count; i++)
             {
-                Cards_FromCandidates[i].CandidatePosition = i;
-                Cards_FromCandidates[i].cardState = Card.state.DeckCandidate;
-                CardCandidatesPositionList.Add(Pos_CandidatesMin.position + (distanceBetween * i));
-                Cards_FromCandidates[i].MoveToPoint(CardCandidatesPositionList[i], Cards_FromCandidates[i].transform.rotation);
+                Cards_ForPick[i].CandidatePosition = i;
+                Cards_ForPick[i].cardState = Card.state.DeckCandidate;
+                CardForPickPositionList.Add(Pos_CandidatesMin.position + (distanceBetween * i));
+                Cards_ForPick[i].MoveToPoint(CardForPickPositionList[i], Cards_ForPick[i].transform.rotation);
             }
         }
     }
@@ -81,7 +92,7 @@ public class DeckEditSystem : MonoBehaviour
         newCard.cardData = cardData;
         newCard.cardState = Card.state.DeckCandidate;
         newCard.loadCard();
-        Cards_FromCandidates.Add(newCard);
+        Cards_ForPick.Add(newCard);
     }
 
     void AddCardToDeck(Card_Basedata card)
