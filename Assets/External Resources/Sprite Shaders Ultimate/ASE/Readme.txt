32a0ad9bf04d9f266e3a9344e257664bd63b20f6
Evan-Lew <evanml.lew@gmail.com>
2023-03-06 23:27:40 -0800

    Revert "Merge branch 'indicator'"
    This reverts commit db72dcbeb52ece7e45d81694c972968dc7937045, reversing
    changes made to 1fe5d87a68ad411b5f45aab160fbfc1b9898199a.

--- Assets/External Resources/Sprite Shaders Ultimate/ASE/Readme.txt
+++
@@ -1,2 +0,0 @@
-This folder contains files required to open my shaders in Amplify Shader Editor.
-You don't need these files or ASE to use the shaders.
---
+++ Assets/testing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    public void testPrint()
+    {
+        Debug.Log("Clicking Button");
+    }
+}
