a0e6a400fa894cd19e7842a892bcc9d9a641ee12
MeltingSlime <113958970+MeltingSlime@users.noreply.github.com>
2023-03-07 20:38:39 -0800

    ray cast needed to be fixed

--- Assets/Scrollbareffect.cs
+++ Assets/Scrollbareffect.cs
@@ -6,12 +6,25 @@ using UnityEngine.EventSystems;
 public class Scrollbareffect : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject Area;
+    public GameObject EndingPoint;
+    bool IsDisplayed = false;
+
+
+    //ray cast 在slider 里面关掉， 背景的Image ray cast 开起来
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Area.SetActive(true);
+        if (!IsDisplayed)
+        {
+            Debug.Log("Hello");
+            Area.SetActive(true);
+            IsDisplayed = true;
+        }
+
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         Area.SetActive(false);
+        IsDisplayed = false;
+
     }
 }
