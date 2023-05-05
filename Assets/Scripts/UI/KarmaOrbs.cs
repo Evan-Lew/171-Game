17c6fa6128d272f247efc37529ebdcc262ee64cc
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2023-05-05 10:32:07 -0700

    They just fuckin spin all over the place

--- Assets/Scripts/UI/KarmaOrbs.cs
+++ Assets/Scripts/UI/KarmaOrbs.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class KarmaOrbs : MonoBehaviour
 {
+
+    public Transform scale;
+    private Vector3 scalePos;
+
+    public float smooth = 0.5f;
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scalePos = scale.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scalePos = scale.position;
+        smoothRotation(scalePos);
+        transform.Translate(transform.forward * speed);
     }
+
+    void smoothRotation(Vector3 target){
+        Vector3 direction = target - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smooth);
+    }
+
 }
