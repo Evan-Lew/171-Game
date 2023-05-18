486bf51798e671a3dd3f3dcbbe433fd129a23e69
Evan-Lew <evanml.lew@gmail.com>
2023-05-17 22:17:52 -0700

    fixing dialogue text

--- Assets/Scripts/Levels Setup/TextManager.cs
+++ Assets/Scripts/Levels Setup/TextManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TextManager : MonoBehaviour
 {
@@ -12,15 +13,37 @@ public class TextManager : MonoBehaviour
     public Queue<string> sentences;
     public Dialogue dialogue;
 
-    public int sentencesLength;
+    [HideInInspector] public int sentencesLength;
+    private bool tutorialLevelLoaded = false;
+    
     void Start()
     {   
         sentences = new Queue<string>();
         StartDialogue(dialogue);
+        IsTutorialLoaded();
     }
+
+    // Helper function to see if the TutorialLevel scene is loaded
+    private void IsTutorialLoaded()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+
+            // Check if the scene is loaded and is not an empty scene
+            if (scene.isLoaded && !string.IsNullOrEmpty(scene.name))
+            {
+                if (scene.name == "TutorialLevel")
+                {
+                    tutorialLevelLoaded = true;
+                }
+            }
+        }
+        Debug.Log(tutorialLevelLoaded);
+    }
+    
     public void StartDialogue(Dialogue moreDialogue)
     {
-       
         sentences.Clear(); 
         foreach (string sentence in moreDialogue.sentences)
         {
@@ -33,26 +56,28 @@ public class TextManager : MonoBehaviour
     {
         sentencesLength = sentences.Count;
         int count = 0;
-        if (sentences.Count == count){
-            scroll.SetActive(false);                //once all messages are read, hides button
         
+        // Disable the scroll after all sentences are read
+        if (sentences.Count == count)
+        {
+            scroll.SetActive(false);
         }
         if (sentences.Count == 0){
-            if (GameController.instance.tutorialIntroDialoguePlaying)
+            // Start the battle after the intro tutorial dialogue
+            if (GameController.instance.tutorialIntroDialoguePlaying && tutorialLevelLoaded)
             {
                 GameController.instance.TutorialIntroDialogueDone();    
             }
 
-            if (GameController.instance.tutorialOutroDialoguePlaying && GameController.instance.tutorialLevelEnd)
+            // End the battle after the end tutorial dialogue
+            if (GameController.instance.tutorialOutroDialoguePlaying && GameController.instance.tutorialLevelEnd && tutorialLevelLoaded)
             {
                 GameController.instance.TutorialOutroDialogueDone(); 
             }
         }
         else
         {
-            
             string sentence = sentences.Dequeue();
-            count += 1;
             StopAllCoroutines();
             StartCoroutine(TypeSentence(sentence));    
         }
